0e9c59c30ea261157f160a34ff2b47a5d5003682
Shrey Nagda <shreynagda2714@gmail.com>
2025-05-24 16:11:19 +0530

    Next lint changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link";
-import { Button } from "./ui/button";
-
 export function Header() {
   return (
     <header className="h-[60px] px-4 md:px-8 bg-black flex justify-center items-center">
